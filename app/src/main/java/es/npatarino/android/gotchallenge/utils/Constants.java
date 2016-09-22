af003a738f43653d9ceb37c2c349417e71d15cb7
manuelgon47 <manuelgon47@gmail.com>
2016-09-22 19:22:44 +0200

    I create a Constants file to define the common constants like a common TAG to debug

---
+++ app/src/main/java/es/npatarino/android/gotchallenge/utils/Constants.java
@@ -0,0 +1,9 @@
+package es.npatarino.android.gotchallenge.utils;
+
+/**
+ * Created by Manuel González Villegas on 22/9/16.
+ */
+public class Constants {
+
+    public static final String TAG_DEBUG = "GOT";
+}
