a931c4b92342f5cffb46832364791856e7588b45
manuelgon47 <manuelgon47@gmail.com>
2016-09-23 07:58:12 +0200

    Extract GotCharacterViewHolder to a separate class

--- app/src/main/java/es/npatarino/android/gotchallenge/characters/GoTAdapter.java
+++ app/src/main/java/es/npatarino/android/gotchallenge/characters/GoTAdapter.java
@@ -2,18 +2,11 @@ package es.npatarino.android.gotchallenge.characters;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -21,6 +14,7 @@ import java.util.List;
 import es.npatarino.android.gotchallenge.DetailActivity;
 import es.npatarino.android.gotchallenge.GoTCharacter;
 import es.npatarino.android.gotchallenge.R;
+import es.npatarino.android.gotchallenge.characters.partials.GotCharacterViewHolder;
 
 /**
  * Created by Manuel González Villegas on 22/9/16.
@@ -43,14 +37,15 @@ public class GoTAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new GotCharacterViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.got_character_row, parent, false));
+        View gotCharacterView = LayoutInflater.from(parent.getContext()).inflate(R.layout.got_character_row, parent, false);
+        return new GotCharacterViewHolder(gotCharacterView, a);
     }
 
     @Override
     public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
         GotCharacterViewHolder gotCharacterViewHolder = (GotCharacterViewHolder) holder;
         gotCharacterViewHolder.render(gcs.get(position));
-        ((GotCharacterViewHolder) holder).imp.setOnClickListener(new View.OnClickListener() {
+        ((GotCharacterViewHolder) holder).getImp().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
                 Intent intent = new Intent(((GotCharacterViewHolder) holder).itemView.getContext(), DetailActivity.class);
@@ -65,41 +60,6 @@ public class GoTAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     @Override
     public int getItemCount() {
         return gcs.size();
-    }
-
-    class GotCharacterViewHolder extends RecyclerView.ViewHolder {
-
-        private static final String TAG = "GotCharacterViewHolder";
-        ImageView imp;
-        TextView tvn;
-
-        public GotCharacterViewHolder(View itemView) {
-            super(itemView);
-            imp = (ImageView) itemView.findViewById(R.id.ivBackground);
-            tvn = (TextView) itemView.findViewById(R.id.tv_name);
-        }
-
-        public void render(final GoTCharacter goTCharacter) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    URL url = null;
-                    try {
-                        url = new URL(goTCharacter.getIu());
-                        final Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-                        a.runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                imp.setImageBitmap(bmp);
-                                tvn.setText(goTCharacter.getN());
-                            }
-                        });
-                    } catch (IOException e) {
-                        Log.e(TAG, e.getLocalizedMessage());
-                    }
-                }
-            }).start();
-        }
     }
 
 }
---
+++ app/src/main/java/es/npatarino/android/gotchallenge/characters/partials/GotCharacterViewHolder.java
@@ -0,0 +1,68 @@
+package es.npatarino.android.gotchallenge.characters.partials;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
+
+import es.npatarino.android.gotchallenge.GoTCharacter;
+import es.npatarino.android.gotchallenge.R;
+
+/**
+ * Created by Manuel González Villegas on 23/9/16.
+ */
+public class GotCharacterViewHolder extends RecyclerView.ViewHolder {
+
+    private static final String TAG = "GotCharacterViewHolder";
+    ImageView imp;
+    TextView tvn;
+    Activity activity;
+
+    public GotCharacterViewHolder(View itemView, Activity activity) {
+        super(itemView);
+        imp = (ImageView) itemView.findViewById(R.id.ivBackground);
+        tvn = (TextView) itemView.findViewById(R.id.tv_name);
+        this.activity = activity;
+    }
+
+    public void render(final GoTCharacter goTCharacter) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                URL url = null;
+                try {
+                    url = new URL(goTCharacter.getIu());
+                    final Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            imp.setImageBitmap(bmp);
+                            tvn.setText(goTCharacter.getN());
+                        }
+                    });
+                } catch (IOException e) {
+                    Log.e(TAG, e.getLocalizedMessage());
+                }
+            }
+        }).start();
+    }
+
+    // **********************************
+    // *        Getters/Setters         *
+    // **********************************
+
+    public ImageView getImp() {
+        return imp;
+    }
+
+    public TextView getTvn() {
+        return tvn;
+    }
+}
