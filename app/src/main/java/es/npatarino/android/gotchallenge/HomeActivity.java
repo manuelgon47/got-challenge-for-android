1ecdee013121a3b2622ed643d86f554119c93529
manuelgon47 <manuelgon47@gmail.com>
2016-09-24 12:14:48 +0200

    Extract the responsability of set the ViewPager to a separate class

--- app/src/main/java/es/npatarino/android/gotchallenge/HomeActivity.java
+++ app/src/main/java/es/npatarino/android/gotchallenge/HomeActivity.java
@@ -1,16 +1,11 @@
 package es.npatarino.android.gotchallenge;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
-    HomeSectionsPagerAdapter spa;
-    ViewPager vp;
     private HomeToolbar toolbar;
-    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,29 +13,13 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         toolbar = new HomeToolbar(this);
-        setSpa(new HomeSectionsPagerAdapter(getSupportFragmentManager()));
 
-        setVp((ViewPager) findViewById(R.id.container));
-        getVp().setAdapter(getSpa());
-
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(getVp());
+        initHomeViewPager();
     }
 
-    public HomeSectionsPagerAdapter getSpa() {
-        return spa;
-    }
+    private HomeViewPager initHomeViewPager() {
 
-    public void setSpa(HomeSectionsPagerAdapter spa) {
-        this.spa = spa;
-    }
-
-    public ViewPager getVp() {
-        return vp;
-    }
-
-    public void setVp(ViewPager vp) {
-        this.vp = vp;
+        return new HomeViewPager(this);
     }
 
 }
---
+++ app/src/main/java/es/npatarino/android/gotchallenge/HomeViewPager.java
@@ -0,0 +1,41 @@
+package es.npatarino.android.gotchallenge;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+
+/**
+ * Created by Manuel González Villegas on 24/9/16.
+ */
+public class HomeViewPager {
+
+    /**
+     * Set the activity to use a ViewPager
+     *
+     * @param activity HomeActivity
+     */
+    public HomeViewPager(HomeActivity activity) {
+        HomeSectionsPagerAdapter spa = getSecctionPagerAdapter(activity);
+
+        ViewPager vp = getViewPager(activity);
+        vp.setAdapter(spa);
+
+        TabLayout tabLayout = getTabLayout(activity);
+        tabLayout.setupWithViewPager(vp);
+
+    }
+
+    private HomeSectionsPagerAdapter getSecctionPagerAdapter(HomeActivity activity) {
+
+        return new HomeSectionsPagerAdapter(activity.getSupportFragmentManager());
+    }
+
+    private ViewPager getViewPager(HomeActivity activity) {
+
+        return (ViewPager) activity.findViewById(R.id.container);
+    }
+
+    private TabLayout getTabLayout(HomeActivity activity) {
+
+        return (TabLayout) activity.findViewById(R.id.tabs);
+    }
+}
