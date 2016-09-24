e1ae68d0e079947ba49f930ef4f211eae20c40f8
manuelgon47 <manuelgon47@gmail.com>
2016-09-24 11:59:24 +0200

    Extract SectionsPagerAdapter to a separate class

--- app/src/main/java/es/npatarino/android/gotchallenge/HomeActivity.java
+++ app/src/main/java/es/npatarino/android/gotchallenge/HomeActivity.java
@@ -2,14 +2,8 @@ package es.npatarino.android.gotchallenge;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-
-import es.npatarino.android.gotchallenge.characters.GoTListFragment;
-import es.npatarino.android.gotchallenge.houses.GoTHousesListFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -47,38 +41,6 @@ public class HomeActivity extends AppCompatActivity {
 
     public void setVp(ViewPager vp) {
         this.vp = vp;
-    }
-
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            if (position == 0) {
-                return new GoTListFragment();
-            } else {
-                return new GoTHousesListFragment();
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Characters";
-                case 1:
-                    return "Houses";
-            }
-            return null;
-        }
     }
 
 }
---
+++ app/src/main/java/es/npatarino/android/gotchallenge/SectionsPagerAdapter.java
@@ -0,0 +1,43 @@
+package es.npatarino.android.gotchallenge;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import es.npatarino.android.gotchallenge.characters.GoTListFragment;
+import es.npatarino.android.gotchallenge.houses.GoTHousesListFragment;
+
+/**
+ * Created by Manuel González Villegas on 24/9/16.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new GoTListFragment();
+        } else {
+            return new GoTHousesListFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Characters";
+            case 1:
+                return "Houses";
+        }
+        return null;
+    }
+}
