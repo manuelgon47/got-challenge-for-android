16dc1e36e69b4c21f3fb67721be76d5b4e336174
manuelgon47 <manuelgon47@gmail.com>
2016-09-25 19:02:42 +0200

    Send to the Log message the catched exception

--- app/src/main/java/es/npatarino/android/gotchallenge/webservice/clients/characters/CharactersClientGotHttpClient.java
+++ app/src/main/java/es/npatarino/android/gotchallenge/webservice/clients/characters/CharactersClientGotHttpClient.java
@@ -31,7 +31,7 @@ public class CharactersClientGotHttpClient implements IGotClientListener {
             List<CharacterDto> characters = parseCharactersJson(jsonArray);
             listener.onResponseOk(characters);
         } catch (JSONException je) {
-            Log.e(Constants.TAG_DEBUG, "Error parsing the Characters json");
+            Log.e(Constants.TAG_DEBUG, "Error parsing the Characters json", e);
             listener.onError();
         }
     }
