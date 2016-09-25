ce619c67727f83bf929e288703728e76f5c0cb68
manuelgon47 <manuelgon47@gmail.com>
2016-09-25 12:35:19 +0200

    DetailActivity go back to prevoius screen

--- app/src/main/java/es/npatarino/android/gotchallenge/DetailActivity.java
+++ app/src/main/java/es/npatarino/android/gotchallenge/DetailActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -53,4 +54,19 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
     }
+    
+    // ******************************
+    // *        Action Bar          *
+    // ******************************
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            super.onBackPressed();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
